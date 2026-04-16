4272af6f71fc2a4284478be34230ab3d834d6a2c
Fahad Bin Siddique <fahad.bin.siddique345@gmail.com>
2026-04-16 18:23:35 +0600

    feat: Add recruiter logo display and job filtering for auth users

--- Job_Portal_App/views.py
+++ Job_Portal_App/views.py
@@ -92,28 +92,32 @@ def profile_update(request):
 
 
 def job_list(request):
-    user=request.user
-    
+    if request.user.is_authenticated:
+        user = request.user
 
-    if user.user_types == 'Recruiter':
-        form_data = JobPostModel.objects.filter(posted_by=user.recruiter_profile)
+        if user.user_types == "Recruiter":
+
+            form_data = JobPostModel.objects.filter(posted_by=user.recruiter_profile)
+
+        else:
+            form_data = JobPostModel.objects.all()
     else:
-        form_data=JobPostModel.objects.all()
+        form_data = JobPostModel.objects.all()
 
     context = {
         "form_data": form_data,
         "title": "Job List",
-        
     }
-    return render(request, "job-list.html",context)
+    return render(request, "job-list.html", context)
 
 
 def job_post(request):
-    user=request.user
+
+    user = request.user
     if request.method == "POST":
         form_data = JobPostForm(request.POST)
         if form_data.is_valid():
-            data=form_data.save(commit=False)
+            data = form_data.save(commit=False)
             data.posted_by = user.recruiter_profile
             data.save()
             return redirect("job_list")
