2caaefaedb71aa920a627308ad21ea637fac9089
Fahad Bin Siddique <fahad.bin.siddique345@gmail.com>
2026-04-21 01:15:00 +0600

    feat: update django project settings and database

--- Job_Portal/settings.py
+++ Job_Portal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -115,8 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
 
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 AUTH_USER_MODEL = "Job_Portal_App.UserInfoModel"
 LOGIN_URL = "login_page"
