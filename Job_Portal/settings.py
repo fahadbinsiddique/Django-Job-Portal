0bfa3b1b92200446d034f7556e2fe614d90b17c4
Fahad Bin Siddique <fahad.bin.siddique345@gmail.com>
2026-04-26 01:20:45 +0600

    build: update static files storage and requirements

--- Job_Portal/settings.py
+++ Job_Portal/settings.py
@@ -117,11 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
+STATIC_URL = "static/"
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_ROOT = BASE_DIR / "staticfiles/"
+MEDIA_ROOT = BASE_DIR / "media/"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 AUTH_USER_MODEL = "Job_Portal_App.UserInfoModel"
 LOGIN_URL = "login_page"
